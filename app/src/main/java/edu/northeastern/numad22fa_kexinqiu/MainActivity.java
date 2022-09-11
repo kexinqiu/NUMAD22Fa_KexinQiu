e296d3838871aa9baff740ddead6c2dca581dbba
Kexin Qiu <kristenqiu19@gmail.con>
2022-09-11 13:06:12 -0400

    button showing message

--- app/src/main/java/edu/northeastern/numad22fa_kexinqiu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_kexinqiu/MainActivity.java
@@ -3,12 +3,30 @@ package edu.northeastern.numad22fa_kexinqiu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button01);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                               "Name: Kexin Qiu" +
+                                       "\nEmail: qiu.ke@northeastern.edu",
+                                Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
