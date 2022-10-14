35b25cf6dc2a96de8e9e8763b15153f21b7886dc
Kexin Qiu <kristenqiu19@gmail.con>
2022-10-14 01:24:09 -0400

    ask for permission

--- app/src/main/java/edu/northeastern/numad22fa_kexinqiu/LocationActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_kexinqiu/LocationActivity.java
@@ -95,6 +95,12 @@ public class LocationActivity extends AppCompatActivity {
             }
         });
 
+        //check
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
 
     };
 
